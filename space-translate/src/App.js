71284c712559a6ba922d38f29758bd40988cc21a
Gashlax <gokmencaglarofc@gmail.com>
2024-03-08 06:35:40 +0300

    History Spit Panel Added

--- space-translate/src/App.js
+++ space-translate/src/App.js
@@ -2,19 +2,27 @@ import './App.css';
 import LanguageInputField from './LanguageInputField.js';
 import LanguageOutputField from './LanguageOutputField.js';
 import React, { useState } from 'react';
+import HistoryPanel from "./HistoryPanel";
 
 
 function App() {
     const [input, setInput] = useState('');
+    const [isHistoryPanelOpen, setHistoryPanelOpen] = useState(false);
 
     const handleInputChange = (event) => {
         setInput(event.target.value);
+    };
+
+    const toggleHistoryPanel = () => {
+        setHistoryPanelOpen(!isHistoryPanelOpen);
     };
 
 
   return (
   <div>
     <LanguageInputField inputValue={input} onInputChange={handleInputChange}/>
+     <div onClick={toggleHistoryPanel}>History</div>
+      {isHistoryPanelOpen && <HistoryPanel />}
     <LanguageOutputField inputValue = {input} />
   </div>
 
---
+++ space-translate/src/HistoryPanel.js
@@ -0,0 +1,15 @@
+import './HistoryPanel.css';
+import React from 'react';
+
+export default function HistoryPanel() {
+    return (
+        <div className={"split-panel"}>
+            <div className="panel-content">
+                Right Panel Content
+                {/* Buraya daha fazla içerik veya bileşen ekleyebilirsiniz */}
+            </div>
+
+        </div>
+    );
+
+}
