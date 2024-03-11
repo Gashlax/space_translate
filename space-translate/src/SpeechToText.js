82df1d7cb7edece649c388a7c21057013a9d28b2
Gashlax <gokmencaglarofc@gmail.com>
2024-03-12 01:16:41 +0300

    Speech to text saving mechanism updated for better history. Only saves after end of the mic close.

--- space-translate/src/SpeechToText.js
+++ space-translate/src/SpeechToText.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import {FaMicrophone, FaMicrophoneSlash} from "react-icons/fa6";
+import './SpeechToText.sass';
 
 const SpeechToText = ( {onRecord} ) => {
     const [isListening, setIsListening] = useState(false);
     const [error, setError] = useState(null);
+    const [currentTranscript, setCurrentTranscript] = useState('');
 
     useEffect(() => {
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -19,16 +21,20 @@ const SpeechToText = ( {onRecord} ) => {
             const currentTranscript = Array.from(event.results)
                 .map(result => result[0].transcript)
                 .join('');
-            onRecord(currentTranscript);
+            onRecord(currentTranscript, false);
+            setCurrentTranscript(currentTranscript);
         };
         recognition.onerror = (event) => {
             setError('Speech recognition error: ' + event.error);
         };
 
         if (isListening) {
+            setCurrentTranscript('');
             recognition.start();
         } else {
             recognition.stop();
+            onRecord(currentTranscript, true);
+            setCurrentTranscript('');
         }
 
         return () => {
@@ -37,11 +43,12 @@ const SpeechToText = ( {onRecord} ) => {
     }, [isListening]);
 
     return (
-        <div>
-            <button onClick={() => setIsListening(!isListening)}>
+        <div className="speech-container">
+            <button className="microphone-button" onClick={() => setIsListening(!isListening)}>
                 {isListening ? <FaMicrophoneSlash style={{color: "red", fontSize:'20px'}}/>
                     :
                     <FaMicrophone style={{color: "green", fontSize:'20px'}}/>}
+                <span className="tooltip-text">{isListening ? "Close Mic" : "Open Mic"}</span>
             </button>
             {error && <p>Error: {error}</p>}
         </div>
